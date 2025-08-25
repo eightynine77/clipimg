9a5c604c7cde5cbad5fbdddbba742239b7ab0784
eightynine77 <theskyisgrey80@proton.me>
2025-08-25 17:48:45 +0700

    modified clipimg.bat

--- clipimg.bat
+++ clipimg.bat
@@ -7,7 +7,9 @@ echo  clipimg - created by jebbidan
 echo ===============================
 echo press [1] key to start clipimg
 echo press [2] key to enable clipimg at startup
-CHOICE /C 12 /N /M ">"
+echo press [3] key to exit
+CHOICE /C 123 /N /M ">"
+if errorlevel 3 exit
 if errorlevel 2 goto startup
 if errorlevel 1 goto runclipimg
 goto home
@@ -15,12 +17,8 @@ goto home
 :runclipimg
 echo.
 echo.
-echo loading...
-powershell -WindowStyle Hidden -Command "Start-Process 'powershell' -ArgumentList '-NoExit','-File','tray.ps1' -WindowStyle Hidden"
-echo.
-echo clipimg is now running.
-echo.
-echo press any key to clear this message...
+start powershell -Command "write-host loading...; Start-Process 'powershell' -ArgumentList '-NoExit','-File','tray.ps1' -WindowStyle Hidden; write-host ''; write-host 'clipimg is now running.';write-host 'press any key to close this window...'; $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')"
+echo press any key to go back...
 pause >nul
 goto home
 
